aad28313110aa8890f80f9e63d67ec20984e84ac
carla <guiasastrologicas@gmail.com>
2026-06-03 08:42:12 -0500

    common fizzbuzz practice

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+const numero = parseInt(prompt("Ingrese un número:"));
+
+for (let i = 1; i <= numero; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
