8637ed281ebcb866cc9abff093e60a2cb192c9a6
carla <guiasastrologicas@gmail.com>
2026-06-05 12:00:07 -0500

    filter

--- ejercicio-filerRange/filter.js
+++ ejercicio-filerRange/filter.js
@@ -4,5 +4,8 @@ that are higher or equal than a and less or equal than b*/
 /* arr.filter(arr, a, b) */
 
 function filterRange(arr, a ,b ){    
-    return arr.filter(numero => {numero >= a && numero <=b;})         
+    return arr.filter(numero => {
+        return numero >= a && numero <=b;})         
 }
+
+console.log(filterRange([3,2,4,7,6], 1, 6));
