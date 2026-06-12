9886752b4bc41fee5df518ec9e321c0570320b86
carla <guiasastrologicas@gmail.com>
2026-06-12 11:37:38 -0500

    cmabios

--- DOM/script.js
+++ DOM/script.js
@@ -11,3 +11,14 @@ sect.appendChild(para);
 const text = document.createTextNode (" — the premier source for web development knowledge.",); 
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
+
+sect.appendChild(linkPara);
+//sect.removeChild(linkPara);
+//linkPara.remove();
+//linkPara.parentNode.removeChild(linkPara);
+
+para.style.color = "white";
+para.style.backgroundColor = "pink";
+para.style.padding = "25px";
+para.style.width= "250px";
+para.style.textAlign = "center";
