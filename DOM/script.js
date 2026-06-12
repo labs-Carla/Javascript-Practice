2c609c03b3d5f0195188045fb41cacc5fd6ba5b9
carla <guiasastrologicas@gmail.com>
2026-06-12 11:25:25 -0500

    create text node

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,13 @@
 const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
+
+const sect = document.querySelector("section");
+
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed your ride. Thank you for visiting us!";
+sect.appendChild(para);
+
+const text = document.createTextNode (" — the premier source for web development knowledge.",); 
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
